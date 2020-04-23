6f408c9408641f7a05b0a6abc25dc37aea959995
anikachristopher <anika.mchristoper@gmail.com>
2020-04-23 16:49:54 -0400

    bundled bcrypt and pry gems

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,6 @@
 class Appointment < ApplicationRecord
   belongs_to :client
   belongs_to :services
+  has_many :reviews
+  has_many :clients, through: :reviews
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,9 +1,11 @@
 class Client < ApplicationRecord
+    has_secure_password
+
     has_many :appointments
     has_many :services, through: :appointments
 
     has_many :reviews
     has_many :reviewed_appointments, through: :reviews, source: :client
-
+    
    
 end
