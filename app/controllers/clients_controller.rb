725cb6e193a5744db47b38ad208476d1939d6f3c
anikachristopher <anika.mchristoper@gmail.com>
2020-04-23 17:07:54 -0400

    create client view

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,13 @@
 class ClientsController < ApplicationController
+
+    def new
+        @client = Client.new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+get '/signup' => 'clients#new'
+post '/signup' => 'clients#create'
+
   resources :services
   resources :reviews
   resources :clients
