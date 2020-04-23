d6389767d281d9ebb692eab88c5ae3e5114b678d
anikachristopher <anika.mchristoper@gmail.com>
2020-04-23 19:42:49 -0400

    built sessions views and included log in and sign up links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,15 @@
 class SessionsController < ApplicationController
+    # def new
+    #     @client = Client.new
+    #     render :login
+    # end
+    
     def home
+    end
+
+    def destroy
+        session.clear
+        redirect_to root_path
     end
 
 end
