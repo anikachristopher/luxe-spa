fbb9083a423f1243598d86d8c34e9afb4c976c8a
anikachristopher <anika.mchristoper@gmail.com>
2020-04-23 17:24:11 -0400

    built login and create methods in the client controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -5,9 +5,23 @@ class ClientsController < ApplicationController
     end
 
     def create
+        @client = Client.new(client_params)
+        if @client.save
+            session[:client_id] = @client.id
+            redirect_to @client
+        else
+            render :new
+        end
     end
 
     def show
     end
+
+    private
+
+    def client_params
+        params.require(:client).permit(:first_name, :last_name, :email, :phone, :password)
+    end
+    
 
 end
