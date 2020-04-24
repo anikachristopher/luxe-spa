8b20ce3910d425bbd26076c1d030577eecc9018c
anikachristopher <anika.mchristoper@gmail.com>
2020-04-23 22:39:43 -0400

    built services controller methods to allow listing, creation and deletion of services

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+    before_action :login_required
+
+    def new
+        @review = Review.new
+    end
+
+    def create
+        @review = current_client.reviews.build(review_params)
+        if @review.save
+            redirect_to appointment_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:content)
+    end
+    
+
+
 end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,29 @@
 class ServicesController < ApplicationController
+    def index
+        @service = Services.all
+    end
+
+    def new
+        @service - Service.new
+    end
+
+    def create
+    @service = Service.new(service_params)
+    if @service.save
+        render :show
+    else
+        render :new
+    end
+
+    def destroy 
+        Service.find(params[:id]).destroy
+        redirect_to client_path(current_client)
+    end 
+
+    private
+
+    def service_params
+        params.require(:service).permit(:name, :duration, :cost)
+    end 
+
 end
