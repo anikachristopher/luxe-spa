6e986be59bed0a5e1b87d87baa1270351ae0c79d
anikachristopher <anika.mchristoper@gmail.com>
2020-04-23 21:07:30 -0400

    built application helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,6 @@
 class SessionsController < ApplicationController
-    # def new
-    #     @client = Client.new
-    #     render :login
-    # end
+    def new
+    end
     
     def home
     end
@@ -10,6 +8,22 @@ class SessionsController < ApplicationController
     def destroy
         session.clear
         redirect_to root_path
+    end
+
+    def create
+        @client = Client.find_by(email: params[:client][:email])
+        if @client && @client.authenticate(params[:client][:password])
+            session[:client_id] = @client.id
+            redirect_to @client
+        else
+            flash[:message] = "Hmm..That Doesn't Look Right. Please try again"
+            redirect_to "/login"
+        end
+    end
+
+    def destroy
+        session.clear
+        redirect_to '/'
     end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+    def current_client
+        @current_client ||= Client.find_by_id(session[:client_id]) if session[:client_id]
+     end
+
+    def is_logged_in?
+        !!session[:client_id]
+    end
+
+    def login_required
+        unless is_logged_in?
+            flash[:error] = "Please log in to create an appointment"
+            redirect_to login_path
+        end
+    end
+
+    def redirect_if_not_logged_in
+        redirect_to '/' if !logged_in?
+      end 
 end
+
